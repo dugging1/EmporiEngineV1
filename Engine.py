d157075b9c5bff0bb316e930dbc0e8603ae858ac
dugging1 <goldminergaming@aol.com>
2014-12-12 22:06:40 +0000

    Setting basics

---
+++ Engine.py
@@ -0,0 +1,89 @@
+__author__ = 'dugging'
+
+
+class IOData():
+    def save(self, folder, file, extension, Data):
+        Export = open(folder+file+extension, 'w')
+        if Data.TYPE == 'PLAYER':
+            Export.write(Data.TYPE)
+            Export.write('\n')
+            Export.write(Data.Name)
+            Export.write(str(Data.Dex))
+            Export.write(str(Data.Int))
+            Export.write(str(Data.Str))
+        Export.close()
+
+    def load(self, folder, file, extension):
+        Import = open(folder+file+extension, 'r')
+        if Import.readline(0) == 'PLAYER':
+            player = Player(Import.readline(1), int(Import.readline(2)), int(Import.readline(3)), int(Import.readline(4)))
+        Import.close()
+        return player
+
+
+class Player():
+    TYPE = 'PLAYER'
+
+    def __init__(self, name, dex, stre, inte):
+        self.Name = name
+        self.Dex = dex
+        self.Str = stre
+        self.Int = inte
+        self.Dam = int(self.Dex/2)+dex
+        self.Def = int(self.Str/8)+int(self.Dex/4)
+        self.MDef = int(self.Str/16)+int(self.Int/4)
+        self.Hp = int(self.Str/4)+stre
+        self.Mp = int(self.Int/8)+inte
+        self.Inv = []
+
+
+class Magic():
+    TYPE = 'MAGIC'
+
+    def __init__(self, name, dam, lvl, pic=None):
+        self.Name = name
+        self.Dam = dam
+        self.Lvl = lvl
+        self.pic = pic
+
+
+class Item():
+    TYPE = 'ITEM'
+    def __init__(self, name, stat, amount, cost, pic=None):
+        self.Name = name
+        self.Stat = stat
+        self.Amount = amount
+        self.Cost = cost
+        self.pic = pic
+
+
+class Mob():
+    TYPE = 'MOB'
+    def __init__(self, name, hp, mp, dam, defe, exp, loot, magic, pic=None):
+        self.Name = name
+        self.Hp = hp
+        self.Mp = mp
+        self.Dam = dam
+        self.Def = defe
+        self.Exp = exp
+        self.Loot = loot
+        self.Magic = magic
+        self.pic = pic
+
+
+class Battle():
+    def __init__(self, Player, Monster):
+        self.Player = Player
+        self.Monster = Monster
+
+    def PlayerPysAtk(self):
+        self.Monster.Hp -= (self.Player.Dam - self.Monster.Def)
+
+    def MonsterPysAtk(self):
+        self.Player.Hp -= (self.Monster.Dam - self.Player.Def)
+
+    def PlayerMAtk(self, Magic):
+        self.Monster.Hp -= (Magic.Dam - int(self.Monster.Def/2))
+
+    def MonsterMAtk(self, Magic):
+        self.Player.Hp -= (Magic.Dam - int(self.Player.MDef))
